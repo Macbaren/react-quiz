ce01a02d994f98f859fdca0e787fbd7352a88e9e
unknown <macbaren124@gmail.com>
2020-11-08 18:50:06 +0300

    add queston

--- src/containers/Ouiz/Quiz.js
+++ src/containers/Ouiz/Quiz.js
@@ -4,24 +4,53 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
   state = {
+    activeQuestion: 0,
     quiz: [
       {
+        question: 'What color is the sky?',
+        rightAnswerId: 3,
+        id: 1,
         answers: [
-          { text: 'Question 1' },
-          { text: 'Question 2' },
-          { text: 'Question 3' },
-          { text: 'Question 4' },
+          { text: 'White', id: 1 },
+          { text: 'Green', id: 2 },
+          { text: 'Blue', id: 3 },
+          { text: 'Yellow', id: 4 },
+        ],
+      },
+      {
+        question: 'What year Mogilev was founded?',
+        rightAnswerId: 3,
+        id: 2,
+        answers: [
+          { text: '1234', id: 1 },
+          { text: '1345', id: 2 },
+          { text: '1273', id: 3 },
+          { text: '1500', id: 4 },
         ],
       },
     ],
+  }
+
+  onAnswerClickHandler = (answerId) => {
+    console.log('Answer Id: ', answerId)
+
+    this.setState({
+      activeQuestion: this.state.activeQuestion + 1,
+    })
   }
 
   render() {
     return (
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
-          <h1>Quiz</h1>
-          <ActiveQuiz answers={this.state.quiz[0].answers} />
+          <h1>Answer all these questions</h1>
+          <ActiveQuiz
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            question={this.state.quiz[this.state.activeQuestion].question}
+            onAnswerClick={this.onAnswerClickHandler}
+            quizLenght={this.state.quiz.length}
+            answerNumber={this.state.activeQuestion + 1}
+          />
         </div>
       </div>
     )
