2fd972cf4c52160007c44c67e1a13ea38d9e3725
unknown <macbaren124@gmail.com>
2020-11-24 11:22:36 +0300

    new branch new

--- src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
+++ src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import classes from './AnswerItem.module.css'
 
 const AnswerItem = (props) => {
+  // create new branch
   const cls = [classes.AnswerItem]
   if (props.state) {
     cls.push(classes[props.state])
