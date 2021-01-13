faf302fd45ce9339c0ed223e4143de78f7462ed2
unknown <macbaren124@gmail.com>
2021-01-13 16:12:22 +0300

    quizCreator

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
+import classes from './QuizCreator.module.css'
+import Button from '../../components/UI/Button/Button'
 
 export default class QuizCreator extends Component {
+  submitHandler = (event) => {
+    event.preventDefault()
+  }
+
+  addQuestionHandler = () => {}
+
+  createQuizHandler = () => {}
+
   render() {
     return (
-      <div>
-        <h1>Quiz Creator</h1>
+      <div className={classes.QuizCreator}>
+        <div>
+          <h1>Test Creator</h1>
+
+          <form onSubmit={this.submitHandler}>
+            <input type="text" />
+            <hr />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+
+            <select></select>
+
+            <Button type="primary" onClick={this.addQuestionHandler}>
+              Add Question
+            </Button>
+            <Button type="success" onClick={this.createQuizHandler}>
+              Create Test
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
