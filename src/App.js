998a8508bda46621bcf1ed61913a2b6e71173b81
unknown <macbaren124@gmail.com>
2021-02-05 17:37:59 +0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from './hoc/Layout/Layout'
 import { Route, Switch } from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
@@ -7,15 +7,35 @@ import QuizCreator from './containers/QuizCreator/QuizCreator'
 import Auth from './containers/Auth/Auth'
 
 function App() {
+  // const state = {
+  //   counter: 0,
+  // }
+
+  // const updateCounter = (value) => {
+  //   setState({
+  //     counter: state.counter + value,
+  //   })
+  // }
+  const [counter, setCounter] = useState(0)
   return (
-    <Layout>
-      <Switch>
-        <Route path="/auth" component={Auth} />
-        <Route path="/quiz-creator" component={QuizCreator} />
-        <Route path="/quiz/:id" component={Quiz} />
-        <Route path="/" component={QuizList} />
-      </Switch>
-    </Layout>
+    // <Layout>
+    //   <Switch>
+    //     <Route path="/auth" component={Auth} />
+    //     <Route path="/quiz-creator" component={QuizCreator} />
+    //     <Route path="/quiz/:id" component={Quiz} />
+    //     <Route path="/" component={QuizList} />
+    //   </Switch>
+    // </Layout>
+    <div className={'App'}>
+      <h1>
+        COUNTER <strong>{counter}</strong>
+      </h1>
+      <hr />
+      <div className="Actions">
+        <button onClick={() => setCounter(counter + 1)}>Add 1</button>
+        <button onClick={() => setCounter(counter - 1)}>Substract 1</button>
+      </div>
+    </div>
   )
 }
 
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,7 @@
+const initialState = {
+  conuter: 0,
+}
+
+export default function rootReducer(state, action) {
+  return state
+}
